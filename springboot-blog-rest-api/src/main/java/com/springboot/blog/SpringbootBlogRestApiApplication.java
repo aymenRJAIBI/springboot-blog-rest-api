0fef3eb7d9e5042a6629ea7ae24bc54660f3a44e
rjaibi <aymenrjaibi2301@gmail.com>
2024-04-17 11:46:19 +0200

    Documentation using OpenAPI (swagger UI)

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,10 +1,33 @@
 package com.springboot.blog;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info (
+				title ="Spring Boot Blog App REST APIs",
+				description = "Spring Boot Blog REST APIs Documentation",
+				version ="v1.0",
+				contact = @Contact(
+							name="Aymen",
+							email ="aymenrjaibi2301@gmail.com"
+				),
+					license = @License(
+							name = "Apache License 2.0"
+					)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog App Documentation",
+				url = "https://github.com/aymenRJAIBI/springboot-blog-rest-api"
+		)
+)
 
 public class SpringbootBlogRestApiApplication {
 
--- springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
@@ -3,6 +3,9 @@ package com.springboot.blog.controller;
 import com.springboot.blog.dto.PostDto;
 import com.springboot.blog.dto.PostResponse;
 import com.springboot.blog.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
+@Tag(name = " CRUD REST APIs for Post Resource")
 public class PostController {
 
     private final PostService postService;
@@ -20,12 +24,28 @@ public class PostController {
         this.postService = postService;
     }
 
+    @Operation(
+            summary = "Create Post REST API",
+            description = "create Post REST API is used to save post into database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "http status 201 created successfully"
+    )
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
 
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get All Posts  REST API",
+            description = "Get All Posts REST API is used to Fetch all  post from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "http status 200 SUCCESSFUL"
+    )
     @GetMapping
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
@@ -37,16 +57,40 @@ public class PostController {
 
     }
 
+    @Operation(
+            summary = "Get Post By Id REST API",
+            description = "Get Post By Id REST API is used to get post from database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "http status 200 SUCCESSFUL"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
+    @Operation(
+            summary = "Update Post  REST API",
+            description = "Update Post REST API is used to update post in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "http status 200 SUCCESSFUL"
+    )
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
         return ResponseEntity.ok(postService.updatePost(id, postDto));
     }
 
+    @Operation(
+            summary = "Delete Post  REST API",
+            description = "Delete Post REST API is used to Delete post from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "http status 200 SUCCESSFUL"
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id) {
         postService.deletePost(id);
@@ -54,7 +98,7 @@ public class PostController {
     }
 
     @GetMapping("category/{id}")
-    public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable(name="id") Long categoryId) {
+    public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable(name = "id") Long categoryId) {
         List<PostDto> result = postService.getPostsByCategory(categoryId);
 
         return ResponseEntity.ok(result);
